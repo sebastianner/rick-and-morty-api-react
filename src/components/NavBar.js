07504c3e4689efd89d1ecbd9a8e4783df260feda
Sebastian <sebastianner23@gmail.com>
2021-09-02 19:28:40 -0500

    2.0.2 img url modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function NavBar(props) {
   return (
     <div className="mb-6">
-      <Link to="/">
+      <Link to="/rick-and-morty-api-react">
         <img
           className="m-0 m-auto"
           src="https://lyricstranslate.com/files/styles/large/public/Rick_and_Morty_logo.png?itok=5No-A7PV"
